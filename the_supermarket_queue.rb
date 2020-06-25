94815ee8603a48750e9f1b526e624989f5efd353
ahtrotta <ahtrotta@gmail.com>
2020-06-25 10:49:20 -0400

    started supermarket queue

---
+++ the_supermarket_queue.rb
@@ -0,0 +1,46 @@
+=begin
+
+*** Understand the Problem ***
+
+problem:
+
+
+input(s):
+customers: array of positive integers where each integer represents a customer and the value represents the amount of time they require to check out
+n: a positive integer representing the number of checkout tills
+
+output(s):
+return an integer, representing the total time required for every customer to checkout
+
+=============================================================================
+
+*** Examples / Test Cases / Edge Cases ***
+
+provided example(s):
+
+test cases:
+
+edge cases:
+
+'bad' input:
+
+=============================================================================
+
+*** Data Structure(s) ***
+
+data structure(s):
+
+
+=============================================================================
+
+*** Algorithm ***
+
+algorithm:
+
+
+=end
+
+
+puts queue_time([5, 3, 4], 1) == 12
+puts queue_time([10, 2, 3, 3], 2) == 10
+puts queue_time([2, 3, 10], 2) == 12
