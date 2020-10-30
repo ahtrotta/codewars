ff5e88f90dc3cad3a5fb34ec6d6287bf9b7766ce
ahtrotta <ahtrotta@gmail.com>
2020-10-30 15:12:37 -0400

    started simple square numbers

---
+++ simple_square_numbers.rb
@@ -0,0 +1,23 @@
+SQUARES = [0, 1, 4, 9]
+
+def solve(n)
+  idx = 1
+  
+  loop do
+  end
+end
+
+p solve(1) == -1
+p solve(2) == -1
+p solve(3) == 1
+p solve(4) == -1
+p solve(5) == 4
+p solve(7) == 9
+p solve(8) == 1    
+p solve(9) == 16
+p solve(10) == -1
+p solve(11) == 25
+p solve(13) == 36
+p solve(17) == 64
+p solve(88901) == 5428900
+p solve(290101) == 429235524
