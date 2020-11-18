11fc5218e2e4db6050c52ff8199a5345df78bfc8
ahtrotta <ahtrotta@gmail.com>
2020-11-18 09:26:02 -0500

    completed oring arrays (for real this time)

--- oring_arrays.rb
+++ oring_arrays.rb
@@ -1,6 +1,22 @@
-def arr_to_bin_str(arr, size)
-  arr.map { |int| sprintf("%0#{size}b", int).chars }
-end
+#def arr_to_bin_str(arr, size)
+#  arr.map { |int| sprintf("%0#{size}b", int).chars }
+#end
+#
+#def or_arrays(arr1, arr2, default=0)
+#  if arr1.size > arr2.size
+#    arr2 << default until arr1.size == arr2.size
+#  elsif arr1.size < arr2.size
+#    arr1 << default until arr1.size == arr2.size
+#  end
+#
+#  pad = (arr1 + arr2).max.to_s(2).length
+#  arr1_bin, arr2_bin = arr_to_bin_str(arr1, pad), arr_to_bin_str(arr2, pad)
+#
+#  arr1_bin.map.with_index do |bin1, i|
+#    b_arr = bin1.zip(arr2_bin[i]).map { |k| k.include?('1') ? '1' : '0' }
+#    b_arr.join.to_i(2)
+#  end
+#end
 
 def or_arrays(arr1, arr2, default=0)
   if arr1.size > arr2.size
@@ -9,13 +25,7 @@ def or_arrays(arr1, arr2, default=0)
     arr1 << default until arr1.size == arr2.size
   end
 
-  pad = (arr1 + arr2).max.to_s(2).length
-  arr1_bin, arr2_bin = arr_to_bin_str(arr1, pad), arr_to_bin_str(arr2, pad)
-
-  arr1_bin.map.with_index do |bin1, i|
-    b_arr = bin1.zip(arr2_bin[i]).map { |k| k.include?('1') ? '1' : '0' }
-    b_arr.join.to_i(2)
-  end
+  arr1.zip(arr2).map { |arr| arr.first | arr.last }
 end
 
 class Test
