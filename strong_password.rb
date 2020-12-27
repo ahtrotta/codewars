32771df33aeb978e06adb537cbf5ee15edbeef1d
ahtrotta <ahtrotta@gmail.com>
2020-12-26 19:21:42 -0500

    completed strong password

--- strong_password.rb
+++ strong_password.rb
@@ -1,3 +1,17 @@
+#def check_password(s)
+#  good = (s.match?(/[a-z]+/) && s.match?(/[A-Z]+/) && s.match?(/\d+/) &&
+#    s.match?(/[!@#$%\^&*?]+/) && s.match?(/\A[a-z\d!@#$%\^&*?]{8,20}\z/i))
+#  good ? 'valid' : 'not valid'
+#end
+
+# practicing with lookaheads
 def check_password(s)
-  s.match?(/\A[a-z\d!@#$%\^&*?]{8,20}\z/) ? 'valid' : 'not valid'
+  s.match?(/\A(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!@#$%\^&*?])[a-zA-Z\d!@#$%\^&*?]{8,20}\z/) ? 'valid' : 'not valid'
 end
+
+p check_password("") == "not valid"
+p check_password("password") == "not valid"
+p check_password("P1@p") == "not valid"
+p check_password("P1@pP1@p") == "valid"
+p check_password("P1@pP1@pP1@pP1@pP1@pP1@p") == "not valid"
+p check_password("Paaaaaa222!!!") == "valid"
